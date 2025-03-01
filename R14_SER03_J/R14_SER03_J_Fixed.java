0c7363bee02fd7f82fc48ee4329387a89719444a
ja566466 <jasuczeme@gmail.com>
2025-03-01 18:41:59 -0500

    Update R14_SER03_J_Fixed.java

--- R14_SER03_J/R14_SER03_J_Fixed.java
+++ R14_SER03_J/R14_SER03_J_Fixed.java
@@ -1,1 +1,23 @@
+import java.io.*;
+
+public class R14_SER03_J_Fixed {
+    public static Object deserialize(String filename) throws IOException, ClassNotFoundException {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
+            Object obj = ois.readObject();
+            if (validateClass(obj)) {
+                return obj;
+            } else {
+                throw new SecurityException("Invalid serialized object");
+            }
+        }
+    }
+
+    private static boolean validateClass(Object obj) {
+        return obj instanceof SafeClass; // Only allow safe classes
+    }
+}
+
+class SafeClass implements Serializable {
+    private static final long serialVersionUID = 1L;
+}
 
