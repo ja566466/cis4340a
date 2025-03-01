73d61e8f8857a9a3f9b5af44fbd2f233ef241dd0
ja566466 <jasuczeme@gmail.com>
2025-03-01 18:35:58 -0500

    Update R02_XP00_J_Defective.java

--- R02_XP00_J/R02_XP00_J_Defective.java
+++ R02_XP00_J/R02_XP00_J_Defective.java
@@ -1,1 +1,9 @@
+import java.io.File;
+
+public class R02_XP00_J_Defective {
+    public void deleteFile() {
+        File someFile = new File("someFileName.txt");
+        someFile.delete();  // No check if deletion was successful
+    }
+}
 
