9c83e8f8c0133eab6a1a0ec0cf44605091810dce
ja566466 <jasuczeme@gmail.com>
2025-03-01 18:39:28 -0500

    Update R06_MET01_J_Fixed.java

--- R06_MET01_J/R06_MET01_J_Fixed.java
+++ R06_MET01_J/R06_MET01_J_Fixed.java
@@ -1,1 +1,14 @@
+public class R06_MET01_J_Fixed {
+    public static int getAbsAdd(int x, int y) {
+        if (x == Integer.MIN_VALUE || y == Integer.MIN_VALUE) {
+            throw new IllegalArgumentException();
+        }
+        int absX = Math.abs(x);
+        int absY = Math.abs(y);
+        if (absX > Integer.MAX_VALUE - absY) {
+            throw new IllegalArgumentException();
+        }
+        return absX + absY;
+    }
+}
 
