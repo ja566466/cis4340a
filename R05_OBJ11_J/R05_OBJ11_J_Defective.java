138ba67ede5be8850367dcb01e868b4b0c942bc6
ja566466 <jasuczeme@gmail.com>
2025-03-01 18:38:13 -0500

    Update R05_OBJ11_J_Defective.java

--- R05_OBJ11_J/R05_OBJ11_J_Defective.java
+++ R05_OBJ11_J/R05_OBJ11_J_Defective.java
@@ -1,1 +1,17 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class R05_OBJ11_J_Defective {
+    private FileInputStream file;
+
+    public R05_OBJ11_J_Defective(String filename) throws IOException {
+        file = new FileInputStream(filename); // Exception can be thrown before full initialization
+    }
+
+    public void close() throws IOException {
+        if (file != null) {
+            file.close();
+        }
+    }
+}
 
