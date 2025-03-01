524db1d5a46decc16bc46abf7187d304eb58cc30
ja566466 <jasuczeme@gmail.com>
2025-03-01 18:36:52 -0500

    Update R04_STR03_J_Defective.java

--- R04_STR03_J/R04_STR03_J_Defective.java
+++ R04_STR03_J/R04_STR03_J_Defective.java
@@ -1,1 +1,12 @@
+import java.math.BigInteger;
+
+public class R04_STR03_J_Defective {
+    public static void main(String[] args) {
+        BigInteger x = new BigInteger("530500452766");
+        byte[] byteArray = x.toByteArray();
+        String s = new String(byteArray); // Incorrect usage
+        byteArray = s.getBytes();
+        x = new BigInteger(byteArray);
+    }
+}
 
