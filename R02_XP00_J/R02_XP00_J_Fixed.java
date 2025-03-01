dc4d60c4ab63ddc6a5495ddbfb1288147f134f11
ja566466 <jasuczeme@gmail.com>
2025-03-01 18:36:09 -0500

    Update R02_XP00_J_Fixed.java

--- R02_XP00_J/R02_XP00_J_Fixed.java
+++ R02_XP00_J/R02_XP00_J_Fixed.java
@@ -1,1 +1,11 @@
+import java.io.File;
+
+public class R02_XP00_J_Fixed {
+    public void deleteFile() {
+        File someFile = new File("someFileName.txt");
+        if (!someFile.delete()) {
+            System.err.println("Failed to delete file!");
+        }
+    }
+}
 
