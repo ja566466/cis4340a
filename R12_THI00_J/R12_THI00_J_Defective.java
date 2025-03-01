6610f2316f769c2fea22818b110c83c20bb345db
ja566466 <jasuczeme@gmail.com>
2025-03-01 18:40:21 -0500

    Update R12_THI00_J_Defective.java

--- R12_THI00_J/R12_THI00_J_Defective.java
+++ R12_THI00_J/R12_THI00_J_Defective.java
@@ -1,1 +1,10 @@
+public class R12_THI00_J_Defective {
+    public static void main(String[] args) {
+        Thread task = new Thread(() -> {
+            System.out.println("Running in: " + Thread.currentThread().getName());
+        });
+
+        task.run(); // Incorrect: Runs in the main thread
+    }
+}
 
