951379ee5e6f18cf4f1fc18c937b52aceea470e1
ja566466 <jasuczeme@gmail.com>
2025-03-01 18:36:37 -0500

    Update R03_NUM03_J_Fixed.java

--- R03_NUM03_J/R03_NUM03_J_Fixed.java
+++ R03_NUM03_J/R03_NUM03_J_Fixed.java
@@ -1,1 +1,9 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+
+public class R03_NUM03_J_Fixed {
+    public static long getInteger(DataInputStream is) throws IOException {
+        return is.readInt() & 0xFFFFFFFFL; // Mask to handle unsigned integers
+    }
+}
 
