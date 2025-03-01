894cdb2563b20d744ee455478f75359cf31f6412
ja566466 <jasuczeme@gmail.com>
2025-03-01 18:41:46 -0500

    Update R14_SER03_J_Defective.java

--- R14_SER03_J/R14_SER03_J_Defective.java
+++ R14_SER03_J/R14_SER03_J_Defective.java
@@ -1,1 +1,10 @@
+import java.io.*;
+
+public class R14_SER03_J_Defective {
+    public static Object deserialize(String filename) throws IOException, ClassNotFoundException {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
+            return ois.readObject(); // Unsafe deserialization
+        }
+    }
+}
 
