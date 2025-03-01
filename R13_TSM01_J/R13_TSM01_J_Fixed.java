3c332691533cec7a3f72739c5f502976431de3a7
ja566466 <jasuczeme@gmail.com>
2025-03-01 18:41:24 -0500

    Update R13_TSM01_J_Fixed.java

--- R13_TSM01_J/R13_TSM01_J_Fixed.java
+++ R13_TSM01_J/R13_TSM01_J_Fixed.java
@@ -1,1 +1,18 @@
+public class R13_TSM01_J_Fixed {
+    private final Thread thread;
+
+    private R13_TSM01_J_Fixed() {
+        thread = new Thread(() -> doWork());
+    }
+
+    public static R13_TSM01_J_Fixed createAndStart() {
+        R13_TSM01_J_Fixed instance = new R13_TSM01_J_Fixed();
+        instance.thread.start(); // Thread starts after object is fully constructed
+        return instance;
+    }
+
+    private void doWork() {
+        System.out.println("Working...");
+    }
+}
 
