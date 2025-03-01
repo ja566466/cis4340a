b3936d154acf65b5a5bcc4abf76874b2cb3591b8
ja566466 <jasuczeme@gmail.com>
2025-03-01 18:37:04 -0500

    Update R04_STR03_J_Fixed.java

--- R04_STR03_J/R04_STR03_J_Fixed.java
+++ R04_STR03_J/R04_STR03_J_Fixed.java
@@ -1,1 +1,12 @@
+import java.math.BigInteger;
+
+public class R04_STR03_J_Fixed {
+    public static void main(String[] args) {
+        BigInteger x = new BigInteger("530500452766");
+        String s = x.toString(); // Valid character data
+        byte[] byteArray = s.getBytes();
+        String ns = new String(byteArray);
+        x = new BigInteger(ns);
+    }
+}
 
