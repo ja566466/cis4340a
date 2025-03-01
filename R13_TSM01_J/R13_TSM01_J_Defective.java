683ec5a346cb5a3dba52c6964c020898d58d47da
ja566466 <jasuczeme@gmail.com>
2025-03-01 18:41:01 -0500

    Update R13_TSM01_J_Defective.java

--- R13_TSM01_J/R13_TSM01_J_Defective.java
+++ R13_TSM01_J/R13_TSM01_J_Defective.java
@@ -1,1 +1,13 @@
+public class R13_TSM01_J_Defective {
+    private final Thread thread;
+
+    public R13_TSM01_J_Defective() {
+        thread = new Thread(() -> doWork());
+        thread.start(); // "this" escapes before constructor is done
+    }
+
+    private void doWork() {
+        System.out.println("Working...");
+    }
+}
 
