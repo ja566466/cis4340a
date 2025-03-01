f80009edcb962b861335418baa7ef9fb02c134c5
ja566466 <jasuczeme@gmail.com>
2025-03-01 18:38:37 -0500

    Update R05_OBJ11_J_Fixed.java

--- R05_OBJ11_J/R05_OBJ11_J_Fixed.java
+++ R05_OBJ11_J/R05_OBJ11_J_Fixed.java
@@ -1,1 +1,22 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class R05_OBJ11_J_Fixed {
+    private FileInputStream file;
+
+    private R05_OBJ11_J_Fixed(FileInputStream file) {
+        this.file = file;
+    }
+
+    public static R05_OBJ11_J_Fixed createInstance(String filename) throws IOException {
+        FileInputStream file = new FileInputStream(filename);
+        return new R05_OBJ11_J_Fixed(file);
+    }
+
+    public void close() throws IOException {
+        if (file != null) {
+            file.close();
+        }
+    }
+}
 
