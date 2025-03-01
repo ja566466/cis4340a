17129cc96c3f1ed90787833d2c1f8ee6dc443384
ja566466 <jasuczeme@gmail.com>
2025-03-01 18:40:36 -0500

    Update R12_THI00_J_Fixed.java

--- R12_THI00_J/R12_THI00_J_Fixed.java
+++ R12_THI00_J/R12_THI00_J_Fixed.java
@@ -1,1 +1,10 @@
+public class R12_THI00_J_Fixed {
+    public static void main(String[] args) {
+        Thread task = new Thread(() -> {
+            System.out.println("Running in: " + Thread.currentThread().getName());
+        });
+
+        task.start(); // Correct: Runs in a new thread
+    }
+}
 
