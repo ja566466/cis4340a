eb972fc21846d2dd662fd0c57e20dbf4d3cadaca
ja566466 <jasuczeme@gmail.com>
2025-03-01 18:35:19 -0500

    Update R00_IDS03_J_Defective.java

--- R00_IDS03_J/R00_IDS03_J_Defective.java
+++ R00_IDS03_J/R00_IDS03_J_Defective.java
@@ -1,1 +1,13 @@
-// This file contains the defective version of IDS03-J (Logging unsanitized input)
+import java.util.logging.Logger;
+
+public class R00_IDS03_J_Defective {
+    private static final Logger logger = Logger.getLogger(R00_IDS03_J_Defective.class.getName());
+
+    public static void logUser(String username, boolean loginSuccessful) {
+        if (loginSuccessful) {
+            logger.severe("User login succeeded for: " + username);
+        } else {
+            logger.severe("User login failed for: " + username);
+        }
+    }
+}
