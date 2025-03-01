7ac298735b29defcfa3179cea5cce3c27b89cb27
ja566466 <jasuczeme@gmail.com>
2025-03-01 18:39:56 -0500

    Update R09_FIO16_J_Defective.java

--- R09_FIO16_J/R09_FIO16_J_Defective.java
+++ R09_FIO16_J/R09_FIO16_J_Defective.java
@@ -1,1 +1,13 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class R09_FIO16_J_Defective {
+    public static void main(String[] args) throws IOException {
+        File file = new File("/tmp/sharedFile.txt"); // Insecure shared location
+        FileWriter writer = new FileWriter(file);
+        writer.write("Sensitive data");
+        writer.close();
+    }
+}
 
