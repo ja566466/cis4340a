63925e2b9f277f4900a36577e98d1e1f351d76c9
ja566466 <jasuczeme@gmail.com>
2025-03-02 12:35:34 -0500

    Update R12_THI00_J_Fixed.java
     Fixed THI00-J: Used Thread.start() instead of Thread.run().

--- R12_THI00_J/R12_THI00_J_Fixed.java
+++ R12_THI00_J/R12_THI00_J_Fixed.java
@@ -1,3 +1,5 @@
+// Fixed version of THI00-J: Used Thread.start() instead of Thread.run().
+
 public class R12_THI00_J_Fixed {
     public static void main(String[] args) {
         Thread task = new Thread(() -> {
